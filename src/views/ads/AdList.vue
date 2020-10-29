2be96fe9185118886b27cc6324596b466c67862b
ValentinAlekhin <headbangernetwork@gmail.com>
2020-10-29 14:49:34 +0300

    Create Ad List page

--- src/views/ads/AdList.vue
+++ src/views/ads/AdList.vue
@@ -1,8 +1,24 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Ad list</h1>
+      <v-flex xs12 sm6 offset-sm-3>
+        <h1 class="text--secondary">My ads</h1>
+        <v-card class="elevation-10 mb-3" v-for="ad in ads" :key="ad.id">
+          <v-layout row>
+            <v-flex xs4>
+              <v-img :src="ad.imgSrc" height="160px" />
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ ad.title }}</h2>
+                <p>{{ ad.description }}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="info" :to="'/ad/' + ad.url">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,6 +26,16 @@
 
 <script>
 export default {
-  data: () => ({}),
+  data: () => ({
+    ads: [
+      {
+        title: 'First ad',
+        description: 'Hello',
+        promo: false,
+        imgSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        id: '1212',
+      },
+    ],
+  }),
 }
 </script>
