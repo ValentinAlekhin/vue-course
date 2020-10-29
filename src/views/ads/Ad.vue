ff01fea78697dfbd8c30bd3252fd7f2c81f095bb
ValentinAlekhin <headbangernetwork@gmail.com>
2020-10-29 14:58:02 +0300

    Create Ad page

--- src/views/ads/Ad.vue
+++ src/views/ads/Ad.vue
@@ -2,7 +2,25 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+            height="300px"
+            alt=""
+          />
+          <v-card-text>
+            <h1 class="text--primary">Lorem</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+              dolores laudantium vitae asperiores earum distinctio iusto natus,
+              blanditiis corrupti laboriosam.
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn>Edit</v-btn>
+            <v-btn class="success">Bye</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
